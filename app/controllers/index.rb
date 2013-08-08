256e628b246c8239683543ad94e04cb9fd64b825
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-08 13:58:52 -0500

    Controllers and Routes Skeleton Complete

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,12 @@
-get '/' do
-  # Look in app/views/index.erb
-  erb :index
+get "/" do
+  erb :"posts/index"
+end
+
+get "/posts/create" do
+end
+
+post "/posts" do
+end
+
+get "/posts/:id" do
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :body, :presence => true
-  validates :title, :presence => true
+  validates :title, :presence => true, length: { minimum: 5 }
   has_many :posts_tags
   has_many :tags, through: :posts_tags
 end
