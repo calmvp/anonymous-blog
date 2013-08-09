49b70838e34acac7bf511de856b05691cdc03763
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-08 19:23:38 -0500

    Mvp complete

--- app/models/post (Shereef Bishay's conflicted copy 2013-08-08).rb
+++
@@ -1,6 +0,0 @@
-class Post < ActiveRecord::Base
-  validates :body, :presence => true
-  validates :title, :presence => true
-  has_many :posts_tags
-  has_many :tags, through: :posts_tags
-end
--- app/models/tag (Shereef Bishay's conflicted copy 2013-08-08).rb
+++
@@ -1,5 +0,0 @@
-class Tag < ActiveRecord::Base
-  validates :title, :presence => true
-  has_many :posts_tags
-  has_many :posts, through: :posts_tags
-end
